7c8ef045c4aed00f0b9626f196cd31280feecd93
Benjamin Reverend <breverend@leadformance.com>
2014-03-25 15:52:38 +0100

    [#65718402] Autodeploy with travis-ci : add listening to travis integration

--- deploy.rb
+++ deploy.rb
@@ -9,12 +9,15 @@ get '/' do
 end
 
 get '/deploy/:stage/:branch' do
-  logfile = "log/deploy-"+Time.now.strftime('%Y%m%d%H%M')+".log"
+  timestamp = Time.now.strftime('%Y%m%d%H%M')
+  logfile = "log/deploy-"+timestamp+".log"
+  loghtml = "log/deploy-"+timestamp+".html"
   cmd = IO.popen("cd ~/apps/bridge ; git pull origin production ; ~/.rvm/bin/bridge_cap "+params[:stage]+" deploy tag="+params[:branch]+" ;")
   log = cmd.readlines
   File.open(logfile, 'w') do |f|
       f.write log.join
   end
-  "Deploy Traces : "+url+logfile
+  html = IO.popen("cat "+logfile+" | bin/ansi2html.sh --bg=dark > "+loghtml)
+  "Deploy Traces : "+url+loghtml
 end
 
