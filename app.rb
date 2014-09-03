8b99cc5a6bb53186cded2a813a2f49e41f160b4f
swanncretin <scretin@leadformance.com>
2014-09-03 17:56:28 +0200

    Fix Jarvis issues

--- app.rb
+++ app.rb
@@ -10,14 +10,25 @@ end
 
 get '/deploy/:stage/:branch' do
   timestamp = Time.now.strftime('%Y%m%d%H%M')
-  logfile = "log/deploy-"+timestamp+".log"
-  loghtml = "log/deploy-"+timestamp+".html"
-  cmd = IO.popen("cd ~/apps/bridge ; git pull origin development; ~/.rvm/bin/bridge_cap "+params[:stage]+" deploy tag="+params[:branch]+" ;")
-  log = cmd.readlines
-  File.open(logfile, 'w') do |f|
-      f.write log.join
-  end
-  html = IO.popen("cat "+logfile+" | bin/ansi2html.sh --bg=dark > "+loghtml)
-  "Deployed ! Traces : "+url+loghtml
-end
+  logfile = "log/deploy-#{timestamp}.log"
+  loghtml = "log/deploy-#{timestamp}.html"
 
+  cmd = "cd ~/apps/bridge;"
+
+  if %w(feature production).include?(params[:stage])
+    cmd += "git checkout development; git pull origin development"
+  else
+    # 2.89 is the latest tag pointing to 1.9.3-p327
+    cmd += "git checkout 2.89"
+  end
+
+  cmd = IO.popen("#{cmd}; #{cap} #{params[:stage]} deploy tag=#{params[:branch]}")
+  log = cmd.readlines
+
+  File.open(logfile, 'w') do |f|
+    f.write log.join
+  end
+
+  html = IO.popen("cat #{logfile} | bin/ansi2html.sh --bg=dark > #{loghtml}")
+  "Deployed ! Traces : #{url}#{loghtml}"
+end
